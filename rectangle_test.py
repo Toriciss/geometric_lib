44c0a29b7a890ddf18b8b555c2a6cf6731aa26d8
Viktoria Obukhova <obu-viktoria@mail.ru>
2023-11-15 22:43:23 +0300

    test: added test for rectangle

---
+++ rectangle_test.py
@@ -0,0 +1,37 @@
+import unittest
+from rectangle import area, perimeter
+class RectangleTestCase(unittest.TestCase):
+    def test_rectangle_area_positive(self):
+        res = area(5, 8)
+        self.assertEqual(res, 40)
+
+    def test_rectangle_area_negative(self):
+        res = area(8, -4)
+        self.assertEqual(res,"Стороны прямоугольника не могут быть отрицательными")
+
+    def test_rectangle_area_real(self):
+        res = area(5.5, 9.9)
+        self.assertEqual(res, 54.45)
+
+    def test_rectangle_area_zero(self):
+        res = area(10, 0)
+        self.assertEqual(res, 0)
+
+    def test_rectangle_perimeter_positive(self):
+        res = perimeter(5, 7)
+        self.assertEqual(res, 24)
+
+    def test_rectangle_perimeter_negative(self):
+        res = perimeter(5, -6)
+        self.assertEqual(res, "Стороны не могут быть отрицательными")
+
+    def test_rectangle_perimeter_real(self):
+        res = perimeter(5.5, 6.6)
+        self.assertEqual(res, 24.2)
+
+    def test_rectangle_perimeter_zero(self):
+        res =  perimeter(5, 0)
+        self.assertEqual(res, 10)
+
+if __name__ == "__main__":
+    unittest.main()
