2b2926eacefabe4c5334a0e727595729cae385e4
Viktoria Obukhova <obu-viktoria@mail.ru>
2023-11-15 23:16:38 +0300

    test: added tests for square

---
+++ square_test.py
@@ -0,0 +1,37 @@
+import unittest
+from square import area, perimeter
+
+class SquareTestCase(unittest.TestCase):
+    def test_square_area_positive(self):
+        res = area(8)
+        self.assertEqual(res, 64)
+
+    def test_square_area_negative(self):
+        res = area(-8)
+        self.assertEqual(res, "The side of the square cannot be negative")
+
+    def test_square_area_real(self):
+        res = area(8.8)
+        self.assertEqual(res, 77.44000000000001)
+
+    def test_square_area_zero(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_square_perimeter_positive(self):
+        res = perimeter(8)
+        self.assertEqual(res, 32)
+
+    def test_square_perimeter_negative(self):
+        res = perimeter(-8)
+        self.assertEqual(res,"The side of the square cannot be negative")
+
+    def test_square_perimeter_real(self):
+        res = perimeter(8.8)
+        self.assertEqual(res, 35.2)
+
+    def test_square_perimeter_zero(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+if __name__ == '__main__':
+    unittest.main()
