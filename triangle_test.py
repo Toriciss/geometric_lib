345449a42a73699a057348f7cb0dd3e6580e5ee1
Viktoria <obu-viktoria@mail.ru>
2023-12-06 13:22:51 +0300

    Update triangle_test.py

--- triangle_test.py
+++ triangle_test.py
@@ -5,10 +5,6 @@ class TriangleTestCase(unittest.TestCase):
         res = area(3, 8)
         self.assertEqual(res, 12)
 
-    def test_triangle_area_negative(self):
-        res = area(10, -8)
-        self.assertEqual(res, "The base and height cannot be negative")
-
     def test_triangle_area_real(self):
         res = area(8.8, 6.6)
         self.assertEqual(res, 29.04)
@@ -20,10 +16,6 @@ class TriangleTestCase(unittest.TestCase):
     def test_triangle_perimeter_positive(self):
         res = perimeter(6, 7, 8)
         self.assertEqual(res,21)
-
-    def test_triangle_perimeter_negative(self):
-        res = perimeter(8, -3, 4)
-        self.assertEqual(res, "The base and height cannot be negative")
 
     def test_triangle_perimeter_real(self):
         res = perimeter(7.7, 2.5, 9)
