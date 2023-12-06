b494c95c54646a67c01b4c8b7bc3c56e21373fea
Viktoria <obu-viktoria@mail.ru>
2023-12-06 13:22:09 +0300

    Update circle_test.py

--- circle_test.py
+++ circle_test.py
@@ -6,10 +6,6 @@ class CircleTestCase(unittest.TestCase):
         res = area(3)
         self.assertEqual(res, 28.274333882308138)
 
-    def test_circle_area_negative(self):
-        res = area(-3)
-        self.assertEqual(res, "The radius cannot be negative")
-
     def test_circle_area_real(self):
         res = area(3.3)
         self.assertEqual(res, 34.21194399759285)
@@ -21,10 +17,6 @@ class CircleTestCase(unittest.TestCase):
     def test_circle_perimeter_positive(self):
         res = perimeter(3)
         self.assertEqual(res, 18.84955592153876)
-
-    def test_circle_perimeter_negative(self):
-        res = perimeter(-3)
-        self.assertEqual(res, "The radius cannot be negative")
 
     def test_circle_perimeter_real(self):
         res = perimeter(3.3)
